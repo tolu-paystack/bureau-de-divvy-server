66a60e65c84a27cef54dc0fa66febce960f61be4
tolu-paystack <tolu@paystack.com>
2020-09-03 10:21:58 +0100

    add getKey function and route for adding subbacount

--- index.js
+++ index.js
@@ -19,21 +19,7 @@ app.use(bodyParser.json())
 app.use(logger("dev"))
 
 app.get("/divvy/subaccounts", (req, res) => {
-  const countryCode = req.query.country
-
-  switch (countryCode) {
-    case "ng":
-      secretKey = process.env.SECRET_KEY_NG
-      break
-    case "gh":
-      secretKey = process.env.SECRET_KEY_GH
-      break
-    case "sa":
-      secretKey = process.env.SECRET_KEY_SA
-      break
-    default:
-      return null
-  }
+  let secretKey = getKey(req.query.country)
 
   fetch(`https://api.paystack.co/subaccount`, {
     headers: {
@@ -47,6 +33,55 @@ app.get("/divvy/subaccounts", (req, res) => {
     .catch((error) => res.status(500).end(error))
 })
 
+app.post("/divvy/subaccount", (req, res) => {
+  let { subaccount, country } = req.body
+  let secretKey = getKey(country)
+
+  fetch(`https://api.paystack.co/subaccount`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${secretKey}`,
+    },
+    body: JSON.stringify(subaccount),
+  })
+    .then((response) => response.json())
+    .then((result) => res.send(result))
+    .catch((error) => res.status(500).end(error))
+})
+
+app.post("/divvy/resolve", (req, res) => {
+  let { accountNumber, bankCode, country } = req.body
+  let secretKey = getKey(country)
+
+  fetch(
+    `https://api.paystack.co/resolve?account_number=${accountNumber}&bank_code=${bankCode}`,
+    {
+      headers: {
+        Authorization: `Bearer ${secretKey}`,
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((resolvedAccount) => res.json(resolvedAccount.data))
+    .catch((error) => res.status(500).end(error))
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
+
+function getKey(country) {
+  switch (country) {
+    case "ng":
+      return process.env.SECRET_KEY_NG
+      break
+    case "gh":
+      return process.env.SECRET_KEY_GH
+      break
+    case "sa":
+      return process.env.SECRET_KEY_SA
+      break
+    default:
+      return null
+  }
+}
